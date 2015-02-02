e4b9efd921a93dafcdf339d5d847e51125cae2c6
Vincent Heuken <me@vincentheuken.com>
2015-02-02 04:56:00 -0800

    added create new room button

--- app.rb
+++ app.rb
@@ -3,10 +3,22 @@ require 'bundler'
 
 Bundler.require
 
+require_relative 'room'
+
 module Moomoo
   class App < Sinatra::Base
     get '/' do
       haml :home, :locals => {:name => "Ron"}
+    end
+
+    get '/rooms/new' do
+      room = Room.new
+      puts room.id
+      redirect to "/rooms/#{room.id}"
+    end
+
+    get '/rooms/:id' do
+      "Hi! This is room #{params[:id]}"
     end
   end
 end
